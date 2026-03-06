d23954ce4edbe30f79899429d6993a5e222f1fdf
Francisco Castillo <cesarfrancisco>
2026-03-06 16:58:28 -0300

    primer commit

---
+++ api/src/main/java/com/carrito/saas/CarritoSaasApplication.java
@@ -0,0 +1,11 @@
+package com.carrito.saas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarritoSaasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CarritoSaasApplication.class, args);
+    }
+}
